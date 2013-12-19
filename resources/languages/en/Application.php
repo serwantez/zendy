fe83525af43e40298c9bfcc11c31237d3388cb99
Piotr Zając <piotr.zajac@efish.pl>
2013-12-19 09:54:59 +0100

    adding languages

---
+++ resources/languages/en/Application.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return array(
+    "Account" => "Account",
+    "Account name" => "Account name",
+    "Application title" => "Pattern",    
+    "Change password" => "Change password",
+    "Help" => "Help",
+    "Help About" => "About",    
+    "Help topics" => "Help topics",
+    "Log in" => "Log in",
+    "Log out" => "Log out",
+    "Options" => "Options",
+    "Password" => "Password",
+    "Profile" => "Profile",
+    "Sidebar" => "Sidebar",
+    "Sign up" => "Sign up",
+    "Toolbar" => "Toolbar",
+    "Tools" => "Tools",
+    "User" => "User",
+    "View" => "View",    
+);
+?>
